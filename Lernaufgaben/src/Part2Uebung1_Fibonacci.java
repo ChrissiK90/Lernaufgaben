e3abedb7ef0324a305ae0829bd6fd66272b8c35f
ChrissiK90 <christina.koren90@gmail.com>
2024-03-10 11:36:45 +0100

    home 10.03.2024

---
+++ Lernaufgaben/src/Part2Uebung1_Fibonacci.java
@@ -0,0 +1,29 @@
+public class Part2Uebung1_Fibonacci {
+
+    //fibonacci
+
+    public static void main(String[] args) {
+
+
+        int fibonacci = 19;
+        int summealt = 0;
+        int summeneu = 1;
+        int ausgabe;
+
+
+        for (int i = 0; i <= fibonacci; i++) {
+            if (i == 0) {                               //Beim ersten Durchlauf habe ich noch keine summealt
+                System.out.print(summeneu + " ");
+            } else {
+                ausgabe = summealt + summeneu;
+                summealt = summeneu;
+                summeneu = ausgabe;
+                System.out.print(ausgabe + " ");
+            }
+
+
+        }
+
+
+    }
+}
