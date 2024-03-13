f3fa720848ccd4d625894c9ec21565773a4a229e
s54824 <christina.koren90@gmail.com>
2024-03-13 09:27:16 +0100

    campus alt 13.3.2024

---
+++ Lernaufgaben/src/NEUPart1Uebung1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.SplittableRandom;
+
+public class NEUPart1Uebung1 {
+
+public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Bitte geben Sie Ihren Namen ein:");
+    String name = scanner.next();
+
+    System.out.println("Bitte geben Sie Ihr alter ein:");
+    int age = scanner.nextInt();
+
+    System.out.println("Hallo " +name+ "! Sie sind " + age+ " Jahre alt.");
+
+
+
+}
+
+}
+
+
